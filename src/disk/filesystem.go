a8b2f82e8bfa5234cc93ecd610c89d63a5e74787
jaked122 <jaked122@gmail.com>
2014-02-09 19:55:16 -0500

    Wrote the filesystem functions. They are likely to be replaced at some point in the future by something more sophisticated, but now they should be testable.

---
+++ src/disk/filesystem.go
@@ -0,0 +1,57 @@
+package disk
+
+import (
+	"os"
+	"bytes"
+)
+
+type SwarmStorage struct{
+	SwarmId string
+	amountused uint64
+}
+
+func  CreateSwarmSystem(swarmid string) *SwarmStorage,*error{
+	var i SwarmId
+	i.SwarmId=swarmid
+	i.amountused=0
+	err:=os.mkdir(swarmid,os.ModeDir)
+	if err{
+		return nil,err
+	}
+	return *i,nil
+}
+
+func (r SwarmStorage) CreateFile(filehash string, length uint64) error{
+	file,err:=os.Create(r.SwarmId)
+
+	if err==nil{
+	defer file.Close()
+		b:=[]byte{0}
+		for i :=range length{
+			_,_=file.Write(b)
+		}
+	}
+	return err
+}
+
+func (r SwarmStorage) WriteFile(filehash string, start uint64, data []byte) error{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
