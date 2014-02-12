51b75270dda1403d5714d1bbead6cae38bdded48
jaked122 <jaked122@gmail.com>
2014-02-12 17:49:35 -0500

    Fixed

--- src/disk/interfaces.go
+++ src/disk/interfaces.go
@@ -4,6 +4,7 @@ type DiskStorage interface{
 	CreateFile(filehash string, length uint64) error
 	ReadFile(filehash string, offset uint64, length uint64) []byte
 	WriteFile(filehash string, start uint64, data []byte) error
+	DeleteFile(filehash string) error
 }
 
 
