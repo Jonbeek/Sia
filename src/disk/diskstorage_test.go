14c52edd98246ec1eb8bd4f4e3a4d59625d7c493
jaked122 <jaked122@gmail.com>
2014-02-14 13:29:41 -0500

    Made changes based upon recommendations from github. Seems to work well. Now it is certainly possible to ensure that the file system works properly.

--- src/disk/diskstorage_test.go
+++ src/disk/diskstorage_test.go
@@ -2,48 +2,53 @@ package disk
 
 import "testing"
 import "os"
-func Test_Swarm(t *testing.T){
-	i,err:=CreateSwarmSystem("SW1")
+
+func Test_Swarm(t *testing.T) {
+	i, err := CreateSwarmSystem("SW1")
 	defer os.Remove("SW1")
-	if err!=nil{
+	if err != nil {
 		t.Error(err.Error())
 	}
-	if i==nil{
+	if i == nil {
 		t.Fatal("first returned object is nil")
 	}
 
-	b,err:=CreateSwarmSystem("SW2")
+	b, err := CreateSwarmSystem("SW2")
 	defer os.Remove("SW2")
-	if err!=nil{
-		t.Error("Failed to create second swarm")
+	if err != nil {
+		if b==nil{
+			t.Error("Failed to create second swarm")
+		}
 	}
-	err=i.CreateFile("hi_there",1000)
-	if err!=nil{
+	_,err = i.CreateFile("hi_there", 1000)
+	if err != nil {
 		t.Error(err.Error())
 		t.Fatal("Failed to Create file")
-		
+
 	}
-	b.CreateFile("Hello",1000)
-	f1:=[]byte{12,34,51,23,51,12,51}
-	f2:=[]byte{24,34,51,25}
-	err=i.WriteFile("hi_there",10,f1)
-	if nil!=err{
-		if os.IsPermission(err){
+	b.CreateFile("Hello", 1000)
+	f1 := []byte{12, 34, 51, 23, 51, 12, 51}
+	f2 := []byte{24, 34, 51, 25}
+	err = i.WriteFile("hi_there", 10, f1)
+	if nil != err {
+		if os.IsPermission(err) {
 			t.Error("Permission bits are set so that things don't work.")
-		}else{
+		} else {
 			t.Error("Failed to write file 1")
 		}
 	}
-	
-	if nil!=b.WriteFile("Hello",0,f2){
+
+	if nil != b.WriteFile("Hello", 0, f2) {
 		t.Error("Error. File 2 failed to write.")
 	}
-	if nil!=i.DeleteFile("hi_there"){
+	if nil != i.DeleteFile("hi_there") {
 		t.Error("Failed to destroy file")
 	}
-	if nil==b.DeleteFile("hello"){
+	if nil!=b.DeleteFile("Hello"){
+		t.Error("Failed to destroy second file")
+	}
+	if nil == b.DeleteFile("hello") {
 		t.Error("Swarm did not cause error when incorrect name applied.")
 	}
 
-	
 }
--- src/disk/filesystem.go
+++ src/disk/filesystem.go
@@ -3,71 +3,98 @@ package disk
 import (
 	"os"
 	//"bytes"
+	"runtime"
 )
 
-type SwarmStorage struct{
-	SwarmId string
+type SwarmStorage struct {
+	SwarmId    string
 	amountused uint64
 }
 
-func  CreateSwarmSystem(swarmid string) (*SwarmStorage,error){
+//This is assuming that if the computer isn't running windows,
+//then it is running linux. A terrible assumption, but it should work in anything
+//either Unix or windows based.
+var useTruncate = runtime.GOOS != "windows"
+
+//helper function to produce the correct filename
+func (r SwarmStorage) getFileName(filehash string) string {
+	return r.SwarmId + string(os.PathSeparator) + filehash
+}
+
+//Opens or creates directory for swarm info, and if it exists, obtains the correct amount of space used by its
+//files
+func CreateSwarmSystem(swarmid string) (r *SwarmStorage, err error) {
 	var i SwarmStorage
 	i.SwarmId = swarmid
-	i.amountused=0
-	err:=os.Mkdir(swarmid,os.ModeDir|os.ModePerm)
-	if err!=nil&&!os.IsExist(err){
-		return nil,err
-	}
-	return &i,nil
-}
-
-func (r SwarmStorage) CreateFile(filehash string, length uint64) error{
-	file,err:=os.Create(r.SwarmId+string(os.PathSeparator)+filehash)
-	defer file.Close()
-	if os.IsExist(err){
-
-	}
-	if err==nil{
-		b:=[]byte{0}
-		for i :=uint64(0);i<length;i++{
-			_,_=file.Write(b)
+	i.amountused = 0
+	err = os.Mkdir(swarmid, os.ModeDir|os.ModePerm)
+	if err != nil {
+		if os.IsExist(err) {
+			o, e := os.Open(swarmid + string(os.PathSeparator))
+			if e != nil {
+			}
+			defer o.Close()
+			fileInfo, e := o.Readdir(-1)
+			for _, fi := range fileInfo {
+				if fi.Mode().IsRegular() {
+					i.amountused += uint64(fi.Size())
+				}
+			}
 		}
-		return nil
 	}
+	r = &i
+	err = err
+	return
+}
+
+func (r SwarmStorage) CreateFile(filehash string, length uint64) (written int64, err error) {
+	file, err := os.Create(r.SwarmId + string(os.PathSeparator) + filehash)
+
+	if err != nil && os.IsExist(err) {
+		//in which case, it should be safe to ignore the error
+		err = nil
+	}
+	defer file.Close()
+	if useTruncate {
+		err = file.Truncate(int64(length))
+	} else {
+		/*
+			Alternative method for windows computers. Not efficient, but it seems to be the best option for computers without
+			the truncate command.
+		*/
+		g := []byte{0}
+		totalsize := int64(0)
+		for i := uint64(0); i < length; i++ {
+			n, err := file.Write(g)
+			if err != nil {
+				break
+			}
+			totalsize += int64(n)
+		}
+		written=totalsize
+	}
+}
+
+func (r SwarmStorage) DeleteFile(filehash string) error {
+	size, err := os.Stat(r.getFileName(filehash))
+	r.amountused -= uint64(size.Size())
+	err = os.Remove(r.SwarmId + string(os.PathSeparator) + filehash)
 	return err
 }
 
-func (r SwarmStorage) DeleteFile(filehash string) error{
-	err:=os.Remove(r.SwarmId+string(os.PathSeparator)+filehash)
-	return err
-}
-
-func (r SwarmStorage) WriteFile(filehash string, start uint64, data []byte) error{
-	path:=r.SwarmId+string(os.PathSeparator)+filehash
-	file,err:=os.Open(path)
-	if err!=nil{
+func (r SwarmStorage) WriteFile(filehash string, start uint64, data []byte) error {
+	path := r.SwarmId + string(os.PathSeparator) + filehash
+	file, err := os.Open(path)
+	if err != nil {
 		return err
 	}
-	file.WriteAt(data,int64(start))
+	file.WriteAt(data, int64(start))
 	file.Close()
 	return nil
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+func (r SwarmStorage) ReadFile(filehash string, start uint64, data []byte) (err error) {
+	file, err := os.Open(r.getFileName(filehash))
+	file.ReadAt( data,int64(start))
+	return
+}
