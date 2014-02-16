6aeb9c39c9a61970d4f18ad7d5244520069b61e8
Colin Rice <colin@daedrum.net>
2014-02-16 02:22:11 -0500

    Wire everything togethor in mainloop

--- src/swarm/stateinformed.go
+++ src/swarm/stateinformed.go
@@ -27,7 +27,15 @@ type StateSwarmInformed struct {
 
 	chain    *BlockChain
 	blockgen <-chan time.Time
-	block    *Block
+
+	sendBroadcast chan struct{}
+	transaction   chan common.Transaction
+	block         chan bwrap
+}
+
+type bwrap struct {
+	block *Block
+	ret   chan State
 }
 
 func NewStateSwarmInformed(chain *BlockChain) (s *StateSwarmInformed) {
@@ -37,10 +45,24 @@ func NewStateSwarmInformed(chain *BlockChain) (s *StateSwarmInformed) {
 
 	s.learning = true
 	s.hostsseen = make(map[string]int)
+	s.sendBroadcast = make(chan struct{})
+	s.transaction = make(chan common.Transaction)
+	s.block = make(chan bwrap)
 
 	go s.broadcastLife()
-	go s.checkBlockGen()
+	go s.mainloop()
 	return
+}
+
+func (s *StateSwarmInformed) HandleTransaction(t common.Transaction) {
+	s.transaction <- t
+}
+
+func (s *StateSwarmInformed) HandleBlock(b *Block) State {
+	c := make(chan State)
+	s.block <- bwrap{b, c}
+	r := <-c
+	return r
 }
 
 func (s *StateSwarmInformed) blockCompiler() (compiler string) {
@@ -60,72 +82,85 @@ func (s *StateSwarmInformed) blockCompiler() (compiler string) {
 	return
 }
 
-func (s *StateSwarmInformed) checkBlockGen() {
+func (s *StateSwarmInformed) mainloop() {
 
 	var compiler string
 
-	for _ = range s.blockgen {
+	for {
+		select {
+		case _ = <-s.sendBroadcast:
+			s.broadcastcount += 1
+			s.chain.outgoingTransactions <- common.TransactionNetworkObject(
+				NewNodeAlive(s.chain.Host, s.chain.Id))
 
-		if s.learning {
-			s.learning = false
-		} else if len(s.chain.BlockHistory) == 0 {
-			s.hostsseen[compiler] += 1
-		}
+		case t := <-s.transaction:
+			s.handleTransaction(t)
 
-		//Dont't try to generate a block if we don't have a majority of hosts
-		if len(s.hostsseen) < 128 {
-			continue
-			//Should actually switch to state swarmdied after a while
-		}
+		case o := <-s.block:
+			n := s.handleBlock(o.block)
+			o.ret <- n
 
-		compiler = s.blockCompiler()
+		case _ = <-s.blockgen:
 
-		if len(s.chain.BlockHistory) == 0 && compiler == s.chain.Host {
-
-			id, err := common.RandomString(8)
-			if err != nil {
-				panic("checkBlockGenRandom" + err.Error())
-			}
-			b := &Block{id, s.chain.Id, s.chain.Host, nil, nil, nil}
-			b.StorageMapping = make(map[string]interface{})
-			for host, _ := range s.hostsseen {
-				b.StorageMapping[host] = nil
+			if s.learning {
+				s.learning = false
+			} else if len(s.chain.BlockHistory) == 0 {
+				s.hostsseen[compiler] += 1
 			}
 
-			s.heartbeats = s.heartbeats[0:0]
-
-			s.chain.outgoingTransactions <- common.BlockNetworkObject(b)
-			s.HandleBlock(b)
-		}
-
-		if len(s.chain.BlockHistory) == 1 && len(s.heartbeats) > 128 {
-
-			id, err := common.RandomString(8)
-			if err != nil {
-				panic(err)
+			//Dont't try to generate a block if we don't have a majority of hosts
+			if len(s.hostsseen) < 128 {
+				continue
+				//Should actually switch to state swarmdied after a while
 			}
 
-			b := &Block{id, s.chain.Id, s.chain.Host, nil, nil, nil}
-			b.StorageMapping = s.chain.BlockHistory[0].StorageMapping
-			b.EntropyStage1 = make(map[string]string)
-			for _, h := range s.heartbeats {
-				b.EntropyStage1[h.Host] = h.Stage1
+			compiler = s.blockCompiler()
+
+			if len(s.chain.BlockHistory) == 0 && compiler == s.chain.Host {
+
+				id, err := common.RandomString(8)
+				if err != nil {
+					panic("checkBlockGenRandom" + err.Error())
+				}
+				b := &Block{id, s.chain.Id, s.chain.Host, nil, nil, nil}
+				b.StorageMapping = make(map[string]interface{})
+				for host, _ := range s.hostsseen {
+					b.StorageMapping[host] = nil
+				}
+
+				s.heartbeats = s.heartbeats[0:0]
+
+				s.chain.outgoingTransactions <- common.BlockNetworkObject(b)
+				s.HandleBlock(b)
 			}
 
-			s.chain.outgoingTransactions <- common.BlockNetworkObject(b)
-			s.HandleBlock(b)
+			if len(s.chain.BlockHistory) == 1 && len(s.heartbeats) > 128 {
 
+				id, err := common.RandomString(8)
+				if err != nil {
+					panic(err)
+				}
+
+				b := &Block{id, s.chain.Id, s.chain.Host, nil, nil, nil}
+				b.StorageMapping = s.chain.BlockHistory[0].StorageMapping
+				b.EntropyStage1 = make(map[string]string)
+				for _, h := range s.heartbeats {
+					b.EntropyStage1[h.Host] = h.Stage1
+				}
+
+				s.chain.outgoingTransactions <- common.BlockNetworkObject(b)
+				s.HandleBlock(b)
+
+			}
 		}
 	}
 }
 
 func (s *StateSwarmInformed) broadcastLife() {
-	s.broadcastcount += 1
-	s.chain.outgoingTransactions <- common.TransactionNetworkObject(
-		NewNodeAlive(s.chain.Host, s.chain.Id))
+	s.sendBroadcast <- struct{}{}
 }
 
-func (s *StateSwarmInformed) HandleTransaction(t common.Transaction) {
+func (s *StateSwarmInformed) handleTransaction(t common.Transaction) {
 	switch n := t.(type) {
 	case *NodeAlive:
 		if !s.learning {
@@ -158,7 +193,7 @@ func (s *StateSwarmInformed) HandleTransaction(t common.Transaction) {
 	}
 }
 
-func (s *StateSwarmInformed) HandleBlock(b *Block) State {
+func (s *StateSwarmInformed) handleBlock(b *Block) State {
 
 	// If the learning timeout hasn't expired, don't accept blocks
 	if s.learning {
