2f1c59c50d15f615f7237dd4a68d990087765768
jaked122 <jaked122@gmail.com>
2014-02-17 13:12:16 -0500

    Initial commit. Wrote the S-Expression data container. Need to write parser. Need to write interpreter at some point as well.

---
+++ src/lisp/lisp.go
@@ -0,0 +1,10 @@
+package lisp
+
+type SExpression struct{
+	data string
+	next *S-Expression
+}
+//Gets a new S-Expression contained in the string
+func ParseSExpression(i string) SExpression{
+	
+}
