48c25b7ccfabfd308f8eb5e1a05b647b4cc27645
jaked122 <jaked122@gmail.com>
2014-02-17 13:40:36 -0500

    Formatted code.

--- src/disk/interfaces.go
+++ src/disk/interfaces.go
@@ -1,7 +1,7 @@
 package disk
 
 type DiskStorage interface {
-	CreateFile(filehash string, length uint64) (int64,error)
+	CreateFile(filehash string, length uint64) (int64, error)
 	ReadFile(filehash string, offset uint64, length uint64) []byte
 	WriteFile(filehash string, start uint64, data []byte) error
 	DeleteFile(filehash string) error
