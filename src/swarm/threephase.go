35589805f533ef750fb25a80446c7025ee158292
Colin Rice <colin@daedrum.net>
2014-03-10 17:21:59 -0400

    Refactor three phase into its own algorithm

---
+++ src/swarm/threephase.go
@@ -0,0 +1,226 @@
+package swarm
+
+import (
+	"common"
+	"log"
+	"sync"
+	"time"
+)
+
+/* A slightly generic implementation of the three phase concensus algorithm
+
+   The first phase is a pause until the designated swarm start time, this allows
+   time for hosts to come online.
+
+   Second each host sends a heartbeat to all other hosts
+
+   Third each hosts signs all heartbeats they have seen and sends them to all
+   other hosts
+
+   Fourth each hosts compiles a block which includes all heartbeats which have
+   been signed by 51% of the network
+
+   The final block is the combination of all heartbeat transactions which have
+   been signed by 51% of the network
+
+*/
+
+type NetHandler interface {
+    SendUpdate(u common.Update)
+    ProduceHeartbeat() common.Update
+    ValidateHeartbeat(h *Heartbeat) bool
+    SignHeartbeat(h *Heartbeat) string
+    ValidateSignature(h *Heartbeat, sig string) bool
+    Id() string
+    Host() string
+    HandleBlock(b *Block)
+}
+
+type ThreePhase struct {
+	lock       *sync.Mutex
+	heartbeats map[string]*Heartbeat
+	signatures map[string]map[string]string
+	nextphase  string
+	phasetimer <-chan time.Time
+    handler NetHandler
+}
+
+func NewThreePhase(starttime time.Time, handler NetHandler) (s *ThreePhase) {
+
+    s = new(ThreePhase)
+	s.phasetimer = time.After(starttime.Sub(time.Now()))
+	s.nextphase = "Heartbeat"
+
+	s.lock = &sync.Mutex{}
+	s.heartbeats = make(map[string]*Heartbeat)
+	s.signatures = make(map[string]map[string]string)
+    s.handler = handler
+
+	go s.mainloop()
+	return
+}
+
+func (s *ThreePhase) Die() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.nextphase = "Dead"
+}
+
+func (s *ThreePhase) mainloop() {
+
+	for _ = range s.phasetimer {
+		log.Print("State: phasetimer fired")
+		s.lock.Lock()
+		switch s.nextphase {
+
+		case "Heartbeat":
+			log.Print("STATE: Heartbeat phase")
+			s.phasetimer = time.Tick(common.STATEINFORMEDDELTA)
+            h := s.handler.ProduceHeartbeat()
+            go s.handler.SendUpdate(h)
+			s.nextphase = "HeartbeatSigning"
+			s.lock.Unlock()
+			s.mainloop()
+			return
+
+		case "HeartbeatSigning":
+			log.Print("STATE: HeartbeatSigning phase")
+			s.phasetimer = time.Tick(common.STATEINFORMEDDELTA)
+			s.produceSignedHeartbeats()
+			s.nextphase = "BlockGeneration"
+
+		case "BlockGeneration":
+			log.Print("STATE: BlockGeneration phase")
+			s.produceBlock()
+			s.nextphase = "BlockDecision"
+
+		case "BlockDecision":
+			log.Print("STATE: BlockDecision phase")
+			ok := s.decideBlock()
+
+			log.Print("STATE: ok=", ok)
+
+			// If we accepted a block we should stop executing and let the
+			// Next state take over
+			if ok {
+				s.nextphase = "Dead"
+			} else {
+				s.nextphase = "Heartbeat"
+			}
+		case "Dead":
+			defer s.lock.Unlock()
+			return
+
+		default:
+			panic("Unrecognized state: " + s.nextphase)
+		}
+		s.lock.Unlock()
+	}
+}
+
+func (s *ThreePhase) produceSignedHeartbeats() {
+
+	signatures := make(map[string]string)
+	for id, h := range s.heartbeats {
+        if ! s.handler.ValidateHeartbeat(h) {
+            continue
+        }
+
+		signatures[id] = s.handler.SignHeartbeat(h)
+	}
+
+	h := NewHeartbeatList(s.handler.Id(), s.handler.Host(), s.heartbeats, signatures)
+	go s.handler.SendUpdate(h)
+}
+
+func (s *ThreePhase) produceBlock() {
+
+	b := NewBlock(s.handler.Id(), s.handler.Host(), s.heartbeats, s.signatures)
+	go s.handler.SendUpdate(b)
+}
+
+func (s *ThreePhase) decideBlock() bool {
+	log.Print("STATE: decideBlock")
+	heartbeats := make(map[string]*Heartbeat)
+	signatures := make(map[string]map[string]string)
+
+	for id, heartbeat := range s.heartbeats {
+		log.Print("Heartbeat id=", id, " len(signatures)=", len(s.signatures[id]))
+		if len(s.signatures[id]) > common.SWARMSIZE/2 {
+			heartbeats[id] = heartbeat
+			signatures[id] = s.signatures[id]
+		}
+	}
+
+	if len(heartbeats) <= common.SWARMSIZE/2 {
+		return false
+	}
+
+	b := NewBlock(s.handler.Id(), s.handler.Host(), s.heartbeats, s.signatures)
+    go s.handler.HandleBlock(b)
+
+	return true
+
+}
+
+func (s *ThreePhase) HandleUpdate(t common.Update) State {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	switch n := t.(type) {
+	case *Heartbeat:
+		if s.nextphase != "HeartbeatSigning" &&
+			s.nextphase != "Heartbeat" {
+			return s
+		}
+
+		s.heartbeats[n.Id] = n
+		log.Print("STATE: Node added")
+
+	case *HeartbeatList:
+		if s.nextphase != "BlockGeneration" &&
+			s.nextphase != "HeartbeatSigning" {
+			return s
+		}
+
+		for id, signature := range n.Signatures {
+            if ! s.handler.ValidateSignature(n.Heartbeats[id], signature) {
+                continue
+            }
+			if _, ok := s.heartbeats[id]; !ok {
+				s.heartbeats[id] = n.Heartbeats[id]
+			}
+			if _, ok := s.signatures[id]; !ok {
+				s.signatures[id] = make(map[string]string)
+			}
+			s.signatures[id][n.Host] = signature
+		}
+
+	case *Block:
+		if s.nextphase != "BlockDecision" &&
+			s.nextphase != "BlockGeneration" {
+			return s
+		}
+
+		for id, signatures := range n.Signatures {
+
+			if _, ok := s.heartbeats[id]; !ok {
+				s.heartbeats[id] = n.Heartbeats[id]
+			}
+			if _, ok := s.signatures[id]; !ok {
+				s.signatures[id] = make(map[string]string)
+			}
+			for host, signature := range signatures {
+                if ! s.handler.ValidateSignature(n.Heartbeats[id], signature) {
+                    continue
+                }
+				s.signatures[id][host] = signature
+			}
+		}
+
+	default:
+
+	}
+	return s
+}
