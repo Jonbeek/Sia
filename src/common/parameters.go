d9f4f5b20398a5fc36157c118eb1b64a27fc3f93
DavidVorick <david.vorick@gmail.com>
2014-04-01 10:13:13 -0500

    forgot to commit constants file

--- src/common/parameters.go
+++ src/common/parameters.go
@@ -4,9 +4,16 @@ import (
 	"time"
 )
 
-var SWARMSIZE int = 128
+// swarmsize is the number of hosts managing each set of files
+var SWARMSIZE int = 192
+
 var STATEINFORMEDDELTA time.Duration = 1 * time.Second
 
 const (
+	// How many bytes of entropy must be produced each entropy cycle
 	ENTROPYVOLUME int = 32
+
+	// How big a single slice of data is for a host, in bytes
+	MINSLICESIZE int = 512
+	MAXSLICESIZE int = 1048576 // 1 MB
 )
