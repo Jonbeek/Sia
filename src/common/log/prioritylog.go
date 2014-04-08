de124ab334d3a187f014e19be68a9d1f859dcf53
Jonathan Beekman <jonbeek@gmail.com>
2014-04-07 22:16:21 -0400

    Document the log package.

--- src/common/log/prioritylog.go
+++ src/common/log/prioritylog.go
@@ -1,3 +1,24 @@
+// Package log implements a simple log with priority levels.
+//
+// There is a difference between a log request and logging for the purposes of
+// this documentation. Logging is the writing of the entry to the Writer where
+// the log is set to output to. A log request tells the log to handle a given
+// message with an associated priority level. When handling an entry, if the
+// priority level is set in the "now" flags of the logger, it is logged
+// immediately. Otherwise, if the "dispose" flag of the logger is NOT set, the
+// entry will be stored to be (potentially) logged on program termination.
+// However, if the "dispose" flag is set, then the message will be discarded.
+//
+// The priority levels supported by this log are Fatal, Error, Warning, Info,
+// and Debug. Aside from determining whether a given message is handled 
+// immediately, the priority level determines the associated "tag" with the 
+// message. 
+//
+// Any message made at the Fatal priority level terminates the program 
+// immediately after making the log request, use it only with errors which
+// cannot be recovered from
+//
+
 package log
 
 import (
@@ -9,6 +30,7 @@ import (
 	"time"
 )
 
+// These are the priority level flags, it is advised to stick with the defaults.
 const (
 	Pfatal = 1 << iota
 	Perror
@@ -16,21 +38,16 @@ const (
 	Pinfo
 	Pdebug
 	PstdFlags = Pfatal | Perror | Pwarning
-	// Use only in the main program.
+	// Use only in the full program for debug mode.
 	PdebugFlags = Pfatal | Perror | Pwarning | Pinfo | Pdebug
 )
 
-// PriorityLog is a log with priority levels.
-// Upon receiving a request to log something, the PriorityLog will:
-// Check to see if the priority level is set in now.
-// If so, print it immediately.
-// Else, check to see if dispose is set
-// If it is, ignore the request.
-// Else, store it in the recordHeap
+// PriorityLog is a log with priority levels. 
 type PriorityLog struct {
-	// now control the levels of priority which are immediately printed
-	// and stored to print in case of an error.
+	//
 	dispose bool
+	// now control the priority levels which are immediately logged and stored
+	// to print in case of an error.
 	now     uint
 	records *recordHeap
 	out     io.Writer
@@ -39,11 +56,13 @@ type PriorityLog struct {
 	// stop stops creation of new log entries.
 	// THere are two mutexes solely to prevent deadlocks.
 	stop sync.Mutex
-	// Every logging function counts as two in pending
-	// The first for formatting the message, the second for creating the record
+	// Every logging function counts as two in pending.
+	// The first for formatting the message, the second for creating the record.
 	pending sync.WaitGroup
 }
 
+// NewPriorityLog creates a PriorityLog with given output Writer, which priority
+// levels to handle immediately, and whether to dispose deferred entries.
 func NewPriorityLog(out io.Writer, flags uint, dispose bool) *PriorityLog {
 	pl := new(PriorityLog)
 	pl.out = out
@@ -68,16 +87,17 @@ func (pl *PriorityLog) Unclaim() {
 }
 
 func (pl *PriorityLog) log(now time.Time, priority uint, message string) {
-	// Calling function called Claim, hopefully
+	// Calling function called Claim, so Unclaim when done.
 	defer pl.Unclaim()
-	// Claim complete use over the variables of pl
+	// Claim complete use over the variables of pl.
 	pl.lock.Lock()
 	defer pl.lock.Unlock()
 	// Optimization!
 	if pl.now&priority != 0 || !pl.dispose {
 		pl.lock.Unlock()
 		// Takes a while.
-		// The calldepth will always be 3, if called directly
+		// The calldepth will always be 3, if called directly.
+		// Caller->helper->log->newRecord
 		rec := newRecord(3, now, priority, message)
 		pl.lock.Lock()
 		if pl.now&priority != 0 {
@@ -89,8 +109,8 @@ func (pl *PriorityLog) log(now time.Time, priority uint, message string) {
 }
 
 // All changes to the state of the log wait for all pending log entries
-// to be logged/stored. In other words, DO NOT USE THESE ANYTIME EXCEPT
-// INITIALIZATION.
+// to be logged/stored. In other words, avoid use except during 
+// initialization.
 
 // SetGlobalFlags sets the the default flag for which priority levels are
 // defered and which ones are handled immediately
@@ -125,9 +145,12 @@ func (pl *PriorityLog) SetOutput(out io.Writer) {
 }
 
 // The following methods log a given message at a given priority level.
-// The priority level of the method should be apparent.
-// The formatting of the message depends on the suffix of the method.
+// There are three styles of formatting, Print, Println, and Printf.
+// Details can be found at http://golang.org/pkg/fmt/
 
+// The following methods use the Print formatting style.
+
+// Log a message at the Fatal priority level, then terminate.
 func (pl *PriorityLog) Fatal(v ...interface{}) {
 	pl.Claim()
 	pl.log(time.Now(), Pfatal, fmt.Sprint(v...))
@@ -136,30 +159,37 @@ func (pl *PriorityLog) Fatal(v ...interface{}) {
 	os.Exit(1)
 }
 
+// Log a message at the Error priority level.
 func (pl *PriorityLog) Error(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Perror, fmt.Sprint(v...))
 }
 
+// Log a message at the Warning priority level.
 func (pl *PriorityLog) Warning(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pwarning, fmt.Sprint(v...))
 }
 
+// Log a message at the Info priority level.
 func (pl *PriorityLog) Info(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pinfo, fmt.Sprint(v...))
 }
 
+// Log a message at the Debug priority level.
 func (pl *PriorityLog) Debug(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pdebug, fmt.Sprint(v...))
 }
 
+// The following methods use the Println formatting style.
+
+// Log a message at the Fatal priority level, then terminate.
 func (pl *PriorityLog) Fatalln(v ...interface{}) {
 	pl.Claim()
 	pl.log(time.Now(), Pfatal, fmt.Sprintln(v...))
@@ -168,30 +198,37 @@ func (pl *PriorityLog) Fatalln(v ...interface{}) {
 	os.Exit(1)
 }
 
+// Log a message at the Error priority level.
 func (pl *PriorityLog) Errorln(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Perror, fmt.Sprintln(v...))
 }
 
+// Log a message at the Warning priority level.
 func (pl *PriorityLog) Warningln(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pwarning, fmt.Sprintln(v...))
 }
 
+// Log a message at the Info priority level.
 func (pl *PriorityLog) Infoln(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pinfo, fmt.Sprintln(v...))
 }
 
+// Log a message at the Debug priority level.
 func (pl *PriorityLog) Debugln(v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pdebug, fmt.Sprintln(v...))
 }
 
+// The following methods use the Printf formatting style.
+
+// Log a message at the Fatal priority level, then terminate.
 func (pl *PriorityLog) Fatalf(format string, v ...interface{}) {
 	pl.Claim()
 	pl.log(time.Now(), Pfatal, fmt.Sprintf(format, v...))
@@ -200,32 +237,36 @@ func (pl *PriorityLog) Fatalf(format string, v ...interface{}) {
 	os.Exit(1)
 }
 
+// Log a message at the Error priority level.
 func (pl *PriorityLog) Errorf(format string, v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Perror, fmt.Sprintf(format, v...))
 }
 
+// Log a message at the Warning priority level.
 func (pl *PriorityLog) Warningf(format string, v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pwarning, fmt.Sprintf(format, v...))
 }
 
+// Log a message at the Info priority level.
 func (pl *PriorityLog) Infof(format string, v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pinfo, fmt.Sprintf(format, v...))
 }
 
+// Log a message at the Debug priority level.
 func (pl *PriorityLog) Debugf(format string, v ...interface{}) {
 	pl.Claim()
 	defer pl.Unclaim()
 	pl.log(time.Now(), Pdebug, fmt.Sprintf(format, v...))
 }
 
+// Log all deferred entries.
 func (pl *PriorityLog) LogStored() {
-	// Prints and clears the log
 	// Stop making new log entries
 	pl.stop.Lock()
 	defer pl.stop.Unlock()
@@ -240,6 +281,7 @@ func (pl *PriorityLog) LogStored() {
 	}
 }
 
+// Dispose of all deferred entries.
 func (pl *PriorityLog) Clear() {
 	// Uncertain if this should wait for all pending records or not.
 	// For now, don't bother.
