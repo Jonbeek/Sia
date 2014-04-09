a6487a2e727b5b9fbdf8555c607249e8da933e36
Luke Champine <luke.champine@gmail.com>
2014-04-09 11:57:39 -0400

    entirely cosmetic

--- src/network/node.go
+++ src/network/node.go
@@ -5,9 +5,10 @@ import (
 	"strconv"
 )
 
-/* serverHandler accepts incoming connections and spawns a clientHandler for each. */
+// serverHandler accepts incoming connections and spawns a clientHandler for each.
 func serverHandler(tcpServ net.Listener, data []byte) {
 	defer tcpServ.Close()
+
 	for {
 		conn, err := tcpServ.Accept()
 		if err != nil {
@@ -18,14 +19,15 @@ func serverHandler(tcpServ net.Listener, data []byte) {
 	}
 }
 
-/* clientHandler reads data sent by a client and processes it. */
-/* It then sends a response based on the client's message. */
+// clientHandler reads data sent by a client and processes it.
+// It then sends a response based on the client's message.
 func clientHandler(conn net.Conn, data []byte) {
 	buffer := make([]byte, 1024)
 	b, err := conn.Read(buffer)
 	if err != nil {
 		return
 	}
+
 	cmd := string(buffer[:b])
 	switch cmd {
 	case "req":
@@ -35,33 +37,36 @@ func clientHandler(conn net.Conn, data []byte) {
 	}
 }
 
-/* SendMessage sends a message over TCP to a specified host and port. */
-/* It then waits for a response, and returns it after closing the connection. */
+// SendMessage sends a message over TCP to a specified host and port.
+// It then waits for a response, and returns it after closing the connection.
 func SendMessage(host string, port int, message []byte) (resp []byte, err error) {
 	conn, err := net.Dial("tcp", host+":"+strconv.Itoa(port))
 	if err != nil {
 		return
 	}
 	defer conn.Close()
+
 	_, err = conn.Write(message)
 	if err != nil {
 		return
 	}
+
 	buffer := make([]byte, 1024)
-	/* TODO: add a timeout here */
+	// TODO: add a timeout here
 	b, err := conn.Read(buffer)
 	resp = buffer[:b]
 	return
 }
 
-/* InitNode initializes a server that listens for TCP connections on a specified port. */
-/* It then spawns a serverHandler with a specified message. */
-/* It is the serverHandler's responsibility to close the TCP connection. */
+// InitNode initializes a server that listens for TCP connections on a specified port.
+// It then spawns a serverHandler with a specified message.
+// It is the serverHandler's responsibility to close the TCP connection.
 func InitNode(port int, data []byte) (err error) {
 	tcpServ, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		return
 	}
+
 	go serverHandler(tcpServ, data)
 	return
 }
