ea6d2d5052e723364cd38c7569f5d6564f92cd66
DavidVorick <david.vorick@gmail.com>
2014-04-09 03:00:57 -0500

    wrote a simple Hash() function to bring back erasure tests

--- src/common/erasure/erasure_test.go
+++ src/common/erasure/erasure_test.go
@@ -1,19 +1,28 @@
 package erasure
 
-/*import (
+import (
 	"bytes"
 	"common"
-	"common/crypto"
+	//"common/crypto"
 	"crypto/rand"   // should use rand from common/crypto
 	"crypto/sha256" // should hash from common/crypto
+	"encoding/hex"  // will be removed after switching to common/crypto
+	"hash"          // will be removed after switching to common/crypto
 	"testing"
 )
+
+// just a patch function so we can run the erasure coding tests
+func Hash(h hash.Hash, data string) string {
+	h.Reset()
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
 // Basic test for reed-solomon coding, verifies that standard input
 // will produce the correct results.
 func TestCoding(t *testing.T) {
 	k := 100
-	m := common.QUORUMSIZE - k
+	m := common.QuorumSize - k
 	bytesPerSegment := 1024
 
 	// generate a random original file
@@ -22,7 +31,7 @@ func TestCoding(t *testing.T) {
 	rand.Read(randomBytes)
 
 	// get hash of original file
-	randomBytesHash := crypto.Hash(sha256.New(), string(randomBytes))
+	randomBytesHash := Hash(sha256.New(), string(randomBytes))
 
 	// encode original file into a data ring
 	ringSegments, err := EncodeRing(k, bytesPerSegment, randomBytes)
@@ -31,7 +40,7 @@ func TestCoding(t *testing.T) {
 	}
 
 	// verify that first k segments are still original data
-	originalDataHash := crypto.Hash(sha256.New(), string(randomBytes))
+	originalDataHash := Hash(sha256.New(), string(randomBytes))
 	if !(bytes.Equal([]byte(originalDataHash), []byte(randomBytesHash))) {
 		t.Fatal("original data was modified after caling EncodeRing!")
 	}
@@ -39,7 +48,7 @@ func TestCoding(t *testing.T) {
 	// reduce file to a set of k segments and print those segments out
 	remainingSegments := make([]string, k)
 	segmentIndicies := make([]uint8, k)
-	for i := m; i < common.QUORUMSIZE; i++ {
+	for i := m; i < common.QuorumSize; i++ {
 		remainingSegments[i-m] = ringSegments[i]
 		segmentIndicies[i-m] = uint8(i)
 	}
@@ -51,7 +60,7 @@ func TestCoding(t *testing.T) {
 	}
 
 	// compare to hash of data when first generated
-	recoveredDataHash := crypto.Hash(sha256.New(), string(recoveredData))
+	recoveredDataHash := Hash(sha256.New(), string(recoveredData))
 	if !(bytes.Equal([]byte(recoveredDataHash), []byte(randomBytesHash))) {
 		t.Fatal("recovered data is different from original data")
 	}
@@ -62,4 +71,4 @@ func TestCoding(t *testing.T) {
 // There should be a fuzzing test that explores random inputs. In particular, I would
 // like to fuzz the 'RebuildSector' function
 
-// There should also be a benchmarking test here.*/
+// There should also be a benchmarking test here.
