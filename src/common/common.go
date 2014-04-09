a2a210ed2370f3b4bb01d1836f30d2dbbb256bbd
DavidVorick <david.vorick@gmail.com>
2014-04-09 01:24:01 -0500

    Switch consts from ALLCAPS to CamelCase

--- src/common/common.go
+++ src/common/common.go
@@ -9,21 +9,23 @@ import (
 
 const (
 	// How many bytes of entropy must be produced each entropy cycle
-	ENTROPYVOLUME int = 32
+	EntropyVolume int = 32
 
 	// How big a single slice of data is for a host, in bytes
-	MINSLICESIZE int = 512
-	MAXSLICESIZE int = 1048576 // 1 MB
+	MinSliceSize int = 512
+	MaxSliceSize int = 1048576 // 1 MB
 
 	// How many hosts participate in each quorum
 	// This number is chosen to minimize the probability of a single quorum
 	// 	becoming more than 80% compromised by an attacker controlling 1/2 of
 	// 	the network.
-	QUORUMSIZE int = 192
+	QuorumSize int = 192
 
 	// How long a single step in the consensus algorithm takes
-	STEPLENGTH time.Duration = 15 * time.Second
+	StepDuration time.Duration = 15 * time.Second
 )
+
+type Entropy [EntropyVolume]byte
 
 // Messages are for sending things over the network.
 // Each message has a single destination, and it is
