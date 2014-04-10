6cba739ce4de33eccc3561e65e6e4b716ed43241
DavidVorick <david.vorick@gmail.com>
2014-04-10 14:44:15 -0500

    lower QuorumSize to 128
    Though I haven't run the strict probabilities, it's pretty clear that
    192 is not going to be feasable for our byzantine algorithms. That's
    36,000 messages per block per host, which is overwhelming. We've reduced
    it to 17,000, which isn't a whole lot better but might be managable.
    Each message should be in the kb range, and can be clumped with 128
    other messages. It's not quite that clean, but in the standard case I
    think we'd see less than 1000 messages per block (because of chunking).

--- src/common/common.go
+++ src/common/common.go
@@ -19,7 +19,7 @@ const (
 	// This number is chosen to minimize the probability of a single quorum
 	// 	becoming more than 80% compromised by an attacker controlling 1/2 of
 	// 	the network.
-	QuorumSize int = 192
+	QuorumSize int = 128
 
 	// How long a single step in the consensus algorithm takes
 	StepDuration time.Duration = 15 * time.Second
