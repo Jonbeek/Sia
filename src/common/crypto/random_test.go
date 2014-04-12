fa170a6421eca6fb86d850a6e11e053d30cfb749
DavidVorick <david.vorick@gmail.com>
2014-04-11 19:50:37 -0500

    added RandomInt() function to crypto

--- src/common/crypto/random_test.go
+++ src/common/crypto/random_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestRandom(t *testing.T) {
+func TestRandomByteSlice(t *testing.T) {
 	randomByteSlice, err := RandomByteSlice(400)
 	if err != nil {
 		t.Fatal(err)
@@ -16,7 +16,17 @@ func TestRandom(t *testing.T) {
 
 	// add a statistical test to verify that the data appears
 	// random
+
+	// there should be a longer test, and perhaps a benchmark
+	// which generates a very large random slice
 }
 
-// there should be a longer test, and perhaps a benchmark
-// which generates a very large random slice
+func TestRandomInt(t *testing.T) {
+	// test 0 as a ceiling
+	zero := RandomInt(0)
+	if zero != 0 {
+		t.Fatal("Expecting rng to produce 0!")
+	}
+
+	// a series of tests that stastically checks for randomness
+}
