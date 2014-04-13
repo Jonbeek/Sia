48c3dd854f3edd8f6f9bc61ff9a64c81e488e1a2
DavidVorick <david.vorick@gmail.com>
2014-04-13 15:23:45 -0500

    Create crypto.CheckKeys(), move to signatures.go

--- src/common/crypto/signatures_test.go
+++ src/common/crypto/signatures_test.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"crypto/rand"
 	"testing"
 )
 
@@ -14,49 +13,10 @@ func TestSigning(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// create an arbitrary message
-	randomMessage := make([]byte, 20)
-	rand.Read(randomMessage)
-
-	// sign the message
-	signedMessage, err := Sign(secretKey, string(randomMessage))
+	err = CheckKeys(publicKey, secretKey)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// verify the message
-	verification, err := Verify(publicKey, signedMessage)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// check that verification succeeded
-	if !verification {
-		t.Fatal("Verification of signature failed!")
-	}
-
-	// create an imposter verification message
-	var imposterMessage SignedMessage
-	imposterMessage.Message = "a"
-	for i := range imposterMessage.Signature {
-		imposterMessage.Signature[i] = byte(8)
-	}
-
-	// try to verify imposter message
-	verification, err = Verify(publicKey, imposterMessage)
-	if verification {
-		t.Fatal("Imposter message was verified as legitimate!")
-	}
-
-	// test signing an empty message
-	signedMessage, err = Sign(secretKey, "")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// test verifying an empty message
-	imposterMessage.Message = ""
-	verification, err = Verify(publicKey, imposterMessage)
 }
 
 // There should probably be some benchmarking
