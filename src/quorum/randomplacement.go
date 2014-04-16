0e85a75a60ed5b1a2f1992a1b291576e334146b6
Evan Thompson <evanmt13@gmail.com>
2014-04-16 17:14:01 -0400

    Fixed randomplacement
    Changed QUORUMSIZE to QuorumSize
    
    modified:
    	randomplacement.go

--- src/quorum/randomplacement.go
+++ src/quorum/randomplacement.go
@@ -7,15 +7,15 @@ import (
 func RandomPlacement(toPlace int, SEED string) []int {
 	ascii := []byte(SEED)
 	intAscii := make([]int, len(ascii))
-	buckets := make([]int, common.QUORUMSIZE)
+	buckets := make([]int, common.QuorumSize)
 	tmp := toPlace
 	index := 0
 	ascInd := 0
 	//splits toPlace into the buckets according to given SEED
 	for tmp != 0 {
 		index += intAscii[ascInd]
-		if index >= common.QUORUMSIZE {
-			index = index - common.QUORUMSIZE
+		if index >= common.QuorumSize {
+			index = index - common.QuorumSize
 		}
 		buckets[index] += 1
 		tmp--
