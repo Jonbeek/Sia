9691b155df6642e0b3bf82465cabdac3654ad323
Luke Champine <luke.champine@gmail.com>
2014-04-17 01:11:03 -0400

    add TCPServer tests

--- src/network/node_test.go
+++ src/network/node_test.go
@@ -1,29 +1,61 @@
 package network
 
 import (
+	"common"
 	"testing"
+	"time"
 )
 
-// TestNetworkNode tests the InitNode and SendMessage functions.
-// InitNode must properly initialize a TCP server.
+// a simple message handler
+type TestMsgHandler struct {
+	result string
+}
+
+func (t *TestMsgHandler) HandleMessage(payload []byte) {
+	t.result = string(payload)
+}
+
+// TestNetworkNode tests the InitServer and SendMessage functions.
+// InitServer must properly initialize a TCP server.
 // SendMessage must succesfully deliver a message.
-// The server must provide the correct response to each message.
-func TestNetworkNode(t *testing.T) {
-	err := InitNode(9988, []byte("foo"))
+func TestTCPServer(t *testing.T) {
+	// create TCPServer and add a message handler
+	tcp := new(TCPServer)
+	tmh := new(TestMsgHandler)
+	tcp.MessageHandlers = make(map[byte]common.MessageHandler)
+	tcp.MessageHandlers[1] = tmh
+	err := tcp.InitServer(9988)
 	if err != nil {
-		t.Fatal("Failed to initialize node:", err)
+		t.Fatal("Failed to initialize TCPServer:", err)
 	}
 
-	resp, err := SendMessage("localhost", 9988, []byte("req"))
+	// send a message to be echoed
+	m := common.Message{common.Address{0, "localhost", 9988}, []byte("\x01hello, world!")}
+	err = tcp.SendMessage(&m)
 	if err != nil {
 		t.Fatal("Failed to send message:", err)
 	}
-	if string(resp) != "foo" {
-		t.Fatal("Bad response: expected 'foo', got", resp)
+	// allow time for message to be processed
+	time.Sleep(10 * time.Millisecond)
+	resp := tmh.result
+	if resp == "" {
+		t.Fatal("Bad response: expected \"hello, world!\", got \"\"")
+	}
+	if string(resp) != "hello, world!" {
+		t.Fatal("Bad response: expected \"hello, world!\", got \"" + string(resp) + "\"")
 	}
 
-	resp, err = SendMessage("localhost", 9988, []byte("cmd"))
-	if string(resp) != "unrecognized command \"cmd\"" {
-		t.Fatal("Bad response, expected 'unrecognized command \"cmd\"', got", resp)
+	// send a message that should not trigger a MessageHandler
+	tmh.result = ""
+	m = common.Message{common.Address{0, "localhost", 9988}, []byte("\xFFwon't be seen")}
+	err = tcp.SendMessage(&m)
+	if err != nil {
+		t.Fatal("Failed to send message:", err)
+	}
+	// allow time for message to be processed
+	time.Sleep(10 * time.Millisecond)
+	resp = tmh.result
+	if resp != "" {
+		t.Fatal("Bad response: expected \"\", got \"" + string(resp) + "\"")
 	}
 }
