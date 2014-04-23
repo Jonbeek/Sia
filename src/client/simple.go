8609bd588e0de5202b3f60a26cc357fc3ffd575a
Jonathan Beekman <jonbeek@gmail.com>
2014-04-22 22:01:23 -0400

    Fix simple.go and limit number of downloads.

--- src/client/simple.go
+++ src/client/simple.go
@@ -10,18 +10,24 @@ type SimpleClient struct {
 	Id Identifier
 	file *os.File
 	done chan bool
+	recv chan bool
 }
 
 func NewSimpleClient(file *os.File) {
 	sc := new(SimpleClient)
 	sc.file = file
 	sc.done = make(chan bool, 1)
+	sc.recv = make(chan bool, 1)
+	sc.recv <- true
+}
 
 func (sc SimpleClient) Identifier() common.Identifier {
 	return sc.Id
 }
 
 func (sc *SimpleClient) HandleMessage(payload []byte) {
+	// Only do it once.
+	<-sc.recv
 	sc.file.Write(payload)
 	sc.done <- true
 }
