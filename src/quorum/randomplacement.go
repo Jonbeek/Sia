e1c11bd78cc756b72e5f625e1d2bd889fa5e3760
evamt13 <evanmt13@gmail.com>
2014-04-27 15:13:10 -0400

    Updated RandomPlacement to account for more corner cases
    The function was not originally checking for negative numbers.
    Now throws an error when a negative number is given.
    
    modified:
    	src/quorum/randomplacement.go
    	src/quorum/randomplacement_test.go

--- src/quorum/randomplacement.go
+++ src/quorum/randomplacement.go
@@ -2,10 +2,14 @@ package quorum
 
 import (
 	"common"
+	"fmt"
 )
 
 func (s *State) RandomPlacement(toPlace int) (buckets []int, err error) {
 	buckets = make([]int, common.QuorumSize)
+	if toPlace < 0 {
+		return buckets, fmt.Errorf("Cannot place a negative number!")
+	}
 	for toPlace != 0 {
 		rand, err := s.randInt(0, common.QuorumSize)
 		if err != nil {
--- src/quorum/randomplacement_test.go
+++ src/quorum/randomplacement_test.go
@@ -14,6 +14,16 @@ func TestRandomPlacement(t *testing.T) {
 		t.Fatal("Failed RandomPlacement of 1")
 	}
 
+	buckets, err = s.RandomPlacement(0)
+	if err != nil {
+		t.Fatal("Failed to place 0!")
+	}
+
+	buckets, err = s.RandomPlacement(-1)
+	if err == nil {
+		t.Fatal("Did not produce error for negative number!")
+	}
+
 	buckets, err = s.RandomPlacement(9000)
 	if err != nil {
 		t.Fatal("Failed RandomPlacement of 9000")
