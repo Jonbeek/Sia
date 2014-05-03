6319c37945181c2af3945c65c87e304fc0b023cd
Luke Champine <luke.champine@gmail.com>
2014-05-03 14:54:18 -0400

    use gob instead of manual marshalling
    this removes the need for messaging tests

--- src/common/messaging.go
+++ src/common/messaging.go
@@ -1,10 +1,5 @@
 package common
 
-import (
-	"encoding/binary"
-	"fmt"
-)
-
 // An Identifier uniquely identifies a participant on a host.
 type Identifier byte
 
@@ -37,48 +32,4 @@ type MessageRouter interface {
 	Address() Address
 	AddMessageHandler(MessageHandler) Address
 	SendMessage(*Message) error
-}
-
-// Turns an Identifier into a []byte
-func (i Identifier) Marshal() (mi []byte) {
-	mi = make([]byte, 1)
-	mi[0] = byte(i)
-	return
-}
-
-// Turns a []byte into an Identifier
-func UnmarshalIdentifier(mi []byte) (i Identifier, err error) {
-	if len(mi) != 1 {
-		err = fmt.Errorf("Marshalled Identifier must be of length 1")
-		return
-	}
-
-	i = Identifier(mi[0])
-	return
-}
-
-// Turns an Address into a []byte
-func (a *Address) Marshal() (ma []byte) {
-	idAndHost := append(a.Id.Marshal(), []byte(a.Host)...)
-	marshalledInt := make([]byte, 4)
-	binary.PutUvarint(marshalledInt, uint64(a.Port))
-	ma = append(idAndHost, marshalledInt...)
-	return
-}
-
-// Turns a []byte into an Address
-func UnmarshalAddress(ma []byte) (a Address, err error) {
-	if len(ma) < 5 {
-		err = fmt.Errorf("marshalledAddress of insufficient length")
-		return
-	}
-
-	a.Id, err = UnmarshalIdentifier(ma[0:1])
-	if err != nil {
-		return
-	}
-	a.Host = string(ma[1 : len(ma)-4])
-	port, _ := binary.Uvarint(ma[len(ma)-4:])
-	a.Port = int(port)
-	return
 }
