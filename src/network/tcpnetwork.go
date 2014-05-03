435ec32f4a487203e37018f283af3ecfc01b05b9
Luke Champine <luke.champine@gmail.com>
2014-05-03 17:25:56 -0400

    initialize TCPServer struct directly

--- src/network/tcpnetwork.go
+++ src/network/tcpnetwork.go
@@ -9,13 +9,13 @@ import (
 
 // TCPServer is a MessageSender that communicates over TCP.
 type TCPServer struct {
-	Addr            common.Address
+	addr            common.Address
 	MessageHandlers []common.MessageHandler
-	Listener        net.Listener
+	listener        net.Listener
 }
 
 func (tcp *TCPServer) Address() common.Address {
-	return tcp.Addr
+	return tcp.addr
 }
 
 // AddMessageHandler adds a MessageHandler to the MessageHandlers slice.
@@ -23,7 +23,7 @@ func (tcp *TCPServer) Address() common.Address {
 // an Address incorporating the identifier.
 func (tcp *TCPServer) AddMessageHandler(mh common.MessageHandler) common.Address {
 	tcp.MessageHandlers = append(tcp.MessageHandlers, mh)
-	addr := tcp.Addr
+	addr := tcp.addr
 	addr.Id = common.Identifier(len(tcp.MessageHandlers) - 1)
 	return addr
 }
@@ -60,17 +60,16 @@ func (tcp *TCPServer) SendMessage(m *common.Message) (err error) {
 // It then spawns a serverHandler with a specified message.
 // It is the serverHandler's responsibility to close the TCP connection.
 func NewTCPServer(port int) (tcp *TCPServer, err error) {
-	tcp = new(TCPServer)
 	tcpServ, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		return
 	}
 
-	// initialize struct fields
-	tcp.Addr = common.Address{0, "localhost", port}
-	// MessageHandlers[0] is reserved for the MessageHandler of the TCPServer
-	tcp.MessageHandlers = make([]common.MessageHandler, 1)
-	tcp.Listener = tcpServ
+	tcp = &TCPServer{
+		common.Address{0, "localhost", port},
+		make([]common.MessageHandler, 1),
+		tcpServ,
+	}
 
 	go tcp.serverHandler()
 	return
@@ -79,13 +78,13 @@ func NewTCPServer(port int) (tcp *TCPServer, err error) {
 // Close closes the connection associated with the TCP server.
 // This causes tcpServ.Accept() to return an err, ending the serverHandler process
 func (tcp *TCPServer) Close() {
-	tcp.Listener.Close()
+	tcp.listener.Close()
 }
 
 // serverHandler accepts incoming connections and spawns a clientHandler for each.
 func (tcp *TCPServer) serverHandler() {
 	for {
-		conn, err := tcp.Listener.Accept()
+		conn, err := tcp.listener.Accept()
 		if err != nil {
 			return
 		} else {
