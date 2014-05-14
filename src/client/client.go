fb06852c934b3ec513f60048b673fb2af13f8f21
Luke Champine <luke.champine@gmail.com>
2014-05-14 03:00:18 -0400

    update client for new Ring

--- src/client/client.go
+++ src/client/client.go
@@ -12,26 +12,39 @@ import (
 // (with apologies to Haskell)
 var (
 	mr       common.MessageRouter
-	SectorDB map[crypto.Hash]*common.Ring
+	SectorDB map[crypto.Hash]*common.RingHeader
 )
 
 // uploadSector splits a Sector into erasure-coded segments and distributes them across a quorum.
 // It creates a Ring from its arguments and stores it in the SectorDB.
-func uploadSector(sec *common.Sector, k int, q common.Quorum) (err error) {
+func uploadSector(sec *common.Sector) (err error) {
+	// look up Sector in SectorDB
+	rh := SectorDB[sec.Hash]
+	if rh == nil {
+		return fmt.Errorf("Sector not found in database")
+	}
+
 	// create ring
-	ring, segs, err := erasure.EncodeRing(sec, k)
+	ring, err := erasure.EncodeRing(sec, rh.Params)
 	if err != nil {
 		return
 	}
-	ring.Hosts = q
 
-	// for now we just send segment i to node i
+	// calculate and store segment hashes
+	for i := range ring {
+		rh.SegHashes[i], err = crypto.CalculateHash(ring[i].Data)
+		if err != nil {
+			return
+		}
+	}
+
+	// for now we just send segment i to host i
 	// this may need to be randomized for security
-	for i := range q {
+	for i := range rh.Hosts {
 		err = mr.SendMessage(&common.Message{
-			Dest: q[i],
+			Dest: rh.Hosts[i],
 			Proc: "Server.UploadSegment",
-			Args: segs[i],
+			Args: ring[i],
 			Resp: nil,
 		})
 		if err != nil {
@@ -39,30 +52,27 @@ func uploadSector(sec *common.Sector, k int, q common.Quorum) (err error) {
 		}
 	}
 
-	// add ring to SectorDB
-	SectorDB[sec.Hash] = ring
-
 	return
 }
 
 // downloadSector retrieves the erasure-coded segments corresponding to a given Sector from a quorum.
 // It reconstructs the original data from the segments and returns the complete Sector
 func downloadSector(hash crypto.Hash) (sec *common.Sector, err error) {
-	// retrieve ring from SectorDB
-	ring := SectorDB[hash]
-	if ring == nil {
-		err = fmt.Errorf("hash not present in database")
+	// look up Sector in SectorDB
+	rh := SectorDB[hash]
+	if rh == nil {
+		err = fmt.Errorf("Sector not found in database")
 		return
 	}
 
 	// send requests to each member of the quorum
 	var segs []common.Segment
-	for i := range ring.Hosts {
+	for i := range rh.Hosts {
 		var seg common.Segment
 		sendErr := mr.SendMessage(&common.Message{
-			Dest: ring.Hosts[i],
+			Dest: rh.Hosts[i],
 			Proc: "Server.DownloadSegment",
-			Args: ring.SegHashes[i],
+			Args: rh.SegHashes[i],
 			Resp: &seg,
 		})
 		if sendErr == nil {
@@ -73,7 +83,7 @@ func downloadSector(hash crypto.Hash) (sec *common.Sector, err error) {
 	}
 
 	// rebuild file
-	sec, err = erasure.RebuildSector(ring, segs)
+	sec, err = erasure.RebuildSector(segs, rh.Params)
 	return
 }
 
@@ -87,22 +97,37 @@ func readQuorumAddresses() (q [common.QuorumSize]common.Address) {
 	return
 }
 
+func generateSector(q common.Quorum) (s *common.Sector, err error) {
+	if q[0].Port == 0 {
+		err = fmt.Errorf("you must connect to a quorum first")
+		return
+	}
+	data, err := crypto.RandomByteSlice(70000)
+	if err != nil {
+		return
+	}
+	s, err = common.NewSector(data)
+	if err != nil {
+		return
+	}
+	SectorDB[s.Hash] = &common.RingHeader{
+		Hosts:  q,
+		Params: s.CalculateParams(common.QuorumSize / 2),
+	}
+	return
+}
+
 func main() {
 	mr, _ = network.NewRPCServer(9989)
 	defer mr.Close()
-	SectorDB = make(map[crypto.Hash]*common.Ring)
+	SectorDB = make(map[crypto.Hash]*common.RingHeader)
 	var (
 		input string
-		q     [common.QuorumSize]common.Address
-		s, rs *common.Sector
-		h, rh crypto.Hash
+		q     common.Quorum
+		s     *common.Sector
+		h     crypto.Hash
+		err   error
 	)
-	data, err := crypto.RandomByteSlice(70000)
-	s, err = common.NewSector(data)
-	h, err = crypto.CalculateHash(data)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
 	for {
 		fmt.Print("Please enter a command: ")
 		fmt.Scanln(&input)
@@ -114,31 +139,40 @@ func main() {
 			fmt.Println("joining quorum")
 			q = readQuorumAddresses()
 			fmt.Println("connected to quorum")
+		case "g":
+			fmt.Println("generating Sector")
+			s, err = generateSector(q)
+			if err != nil {
+				fmt.Println("error:", err)
+				fmt.Println("failed to generate Sector")
+				break
+			}
+			h = s.Hash
+			fmt.Println("created Sector with hash", h[:10])
 		case "u":
 			fmt.Println("uploading file")
-			err = uploadSector(s, 2, q)
+			err = uploadSector(s)
 			if err != nil {
 				fmt.Println("error:", err)
 				fmt.Println("upload failed")
 				break
 			}
 			fmt.Println("upload successful")
-			fmt.Println("hash:", h[:])
 		case "d":
 			fmt.Println("downloading file")
-			rs, err = downloadSector(h)
+			rs, err := downloadSector(h)
 			if err != nil {
 				fmt.Println("error:", err)
 				fmt.Println("download failed")
 				break
 			}
-			rh, err = crypto.CalculateHash(rs.Data)
+			rh, err := crypto.CalculateHash(rs.Data)
 			if err != nil {
 				fmt.Println("error:", err)
 				break
 			}
 			fmt.Println("download successful")
-			fmt.Println("hash:", rh[:])
+			fmt.Println("hash:", rh[:10])
 		case "q":
 			return
 		}
